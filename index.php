cfec85510a71cd15b3ea1ea734142b68ecc0b6fe
Alberto Ferrer <albertoferrer@gmail.com>
2015-09-22 21:15:55 -0500

    TAB Fix

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if(!in_array($Path[0], $allowed))
 switch($Path[0])
 {
       default:
-	$tpl->assign('title', 'Dealers Will Hear Car Talk at Noon.');
-        $tpl->display('index.tpl');
+				$tpl->assign('title', 'Dealers Will Hear Car Talk at Noon.');
+				$tpl->display('index.tpl');
       break;
 }
